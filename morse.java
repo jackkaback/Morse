4664238212c52d324e98f6561da46b45d603e433
jackkaback <jackkaback@gmail.com>
2018-09-22 11:38:59 -0400

    Started map

--- morse.java
+++ morse.java
@@ -1,7 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class morse {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("a", "*- ");
+        map.put("b", "-*** ");
+        map.put("c", "-*-* ");
+        map.put("d", "-** ");
+        map.put("e", "* ");
+        map.put("f", "**-* ");
+        map.put("g", "--* ");
+        map.put("h", "**** ");
+        map.put("i", "** ");
+        map.put("j", "*--- ");
+
+        System.out.println(map.get("a"));
     }
 
 }

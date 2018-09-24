da2985f43f5358aaf11c96708b5a4fde73eb6b82
jackkaback <jackkaback@gmail.com>
2018-09-24 11:32:00 -0400

    Added start to C++

---
+++ morse.cpp
@@ -0,0 +1,12 @@
+//
+// Created by jack on 9/24/18.
+//
+
+#include<iostream>
+
+int main(){
+
+    std::cout << "Hello world" << std::endl;
+
+    return 0;
+}
---
+++ morseC++.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+g++ morse.cpp
+./a.out

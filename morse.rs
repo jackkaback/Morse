ebb586471fe920843683953e05d09b7bacf7dfa0
jackkaback <jackkaback@gmail.com>
2018-09-26 01:29:27 -0400

    started rust dictonary

--- morse.rs
+++ morse.rs
@@ -1,3 +1,9 @@
+use std::collections::HashMap;
+
 fn main() {
+    let mut morseDict = HashMap::new();
+
+    morseDict.insert("a".to_string(), "*- ".to_string());
+
     println!("Hello World!");
 }

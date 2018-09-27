a32131ba3dbb4dc2f954a0942ea3a8b154f7da9e
jackkaback <jackkaback@gmail.com>
2018-09-27 10:10:33 -0400

    added morseDict

--- morse.go
+++ morse.go
@@ -4,4 +4,48 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello world")
+
+	var morseDict map[string]string
+	morseDict = make(map[string]string)
+
+	morseDict["a"] = "*- "
+	morseDict["b"] = "-*** "
+	morseDict["c"] = "-*-* "
+	morseDict["d"] = "-** "
+	morseDict["e"] = "* "
+	morseDict["f"] = "**-* "
+	morseDict["g"] = "--* "
+	morseDict["h"] = "**** "
+	morseDict["i"] = "** "
+	morseDict["j"] = "*--- "
+	morseDict["k"] = "-*- "
+	morseDict["l"] = "*-** "
+	morseDict["m"] = "-- "
+	morseDict["n"] = "-* "
+	morseDict["o"] = "--- "
+	morseDict["p"] = "*--* "
+	morseDict["q"] = "--*- "
+	morseDict["r"] = "*-* "
+	morseDict["s"] = "*** "
+	morseDict["t"] = "- "
+	morseDict["u"] = "**- "
+	morseDict["v"] = "***- "
+	morseDict["w"] = "*-- "
+	morseDict["x"] = "-**- "
+	morseDict["y"] = "-*-- "
+	morseDict["z"] = "--** "
+	morseDict["0"] = "----- "
+	morseDict["1"] = "*---- "
+	morseDict["2"] = "**--- "
+	morseDict["3"] = "***-- "
+	morseDict["4"] = "****- "
+	morseDict["5"] = "***** "
+	morseDict["6"] = "-**** "
+	morseDict["7"] = "--*** "
+	morseDict["8"] = "---** "
+	morseDict["9"] = "----* "
+	morseDict["."] = "*-*-*- "
+	morseDict["?"] = "**--** "
+	morseDict[" "] = "\t"
+
 }
